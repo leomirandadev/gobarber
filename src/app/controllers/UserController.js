12c4625777239a2d0b6702416a780918091b5162
Leonardo Miranda <leoamiranda2@gmail.com>
2019-08-29 10:51:33 -0300

    validação com a lib yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,18 +1,36 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: 'Validation fails',
+      });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
       return res.status(400).json({
-        message: 'E-mail já existe',
+        message: 'E-mail already exists.',
       });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({
-      message: 'Cadastrado com sucesso',
+      message: 'User was created!',
       output: {
         id,
         name,
@@ -23,6 +41,29 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number()
+        .integer()
+        .required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: 'Validation fails',
+      });
+    }
+
     const { id, email, oldPassword } = req.body;
     const user = await User.findByPk(id);
     if (email !== user.email) {
@@ -30,19 +71,19 @@ class UserController {
 
       if (userExists) {
         return res.status(400).json({
-          message: 'E-mail já existe',
+          message: 'E-mail already exists',
         });
       }
     }
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(400).json({
-        message: 'Senha antiga inválida',
+        message: 'Password does not match',
       });
     }
 
     const { name, provider } = await user.update(req.body);
     return res.json({
-      message: 'Cadastrado com sucesso',
+      message: 'Updated was success',
       output: {
         id,
         name,
